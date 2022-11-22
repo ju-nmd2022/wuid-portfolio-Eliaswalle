39ac89380ece3c448b0fe1ede7dc24a4b0504320
Eliaswalle <wael22nq@student.ju.se>
2022-11-22 10:40:46 +0100

    animation
    animation to portfolio and text!

--- portfolio.js
+++ portfolio.js
@@ -3,3 +3,17 @@ const hamburger = document.querySelector(".hamburger");
 hamburger.addEventListener("click", function () {
   this.classList.toggle("is-active");
 });
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
